c46d4d4aeaac2d3584b94a26057f60399152ec65
PopSockets <web@popsockets.com>
2018-06-30 13:38:35 -0600

    getting started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Customizer from './customizer/customizer.js'
+
 class App extends Component {
+  state = {
+
+  }
+
   render() {
     return (
       <div className="App">
-
+        
       </div>
     );
   }
---
+++ src/customizer/customizer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Customizer extends Component {
+  createContainer = (props) => {
+      return (
+        <h1>derp</h1>
+      )
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+export default Customizer;
